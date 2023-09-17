32708df45c5428e4464db617e6cb906c0caf10c9
Mucahit <172803037@ogr.cbu.edu.tr>
2023-09-18 00:28:07 +0300

    login with google button

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,19 @@
 <template>
-    <h2>Login</h2>
+    <main class="form-signin m-auto w-100">
+        <div class="row">
+            <div class="col-12">
+                <button class="btn btn-primary">
+                    <i class="bi bi-google"></i> ile Giriş
+                </button>
+            </div>
+        </div>
+    </main>
 </template>
+
+<style scoped>
+.form-signin{
+    max-width: 330px;
+    padding: 15px;
+    padding-top: 140px;
+}
+</style>
